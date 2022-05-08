24486467adeba946096ce2721f6c1519a35dfa9d
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-08 12:51:59 +0300

    add comment,pitch votes models

--- app/models.py
+++ app/models.py
@@ -10,15 +10,35 @@ class User(db.Model):
     def __repr__(self):
         return f'User {self.username}'
     
+
+class Pitch(db.Model):
+    __tablename__='pitches'
+    id = db.Column(db.Integer, primary_key= True)
+    comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category = db.Column(db.String(255), index = True,nullable = False) 
     
-class Categories():
-    ...
+    def __repr__(self):
+        return f'Pitch {self.post}'
     
-class Piches:
-    ...
+class Votes(db.Model):
+    __tablename__='votes'
+    id = db.Column(db.Integer, primary_key= True) 
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     
-class Votes():
-    ... 
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
            
-class Comments():
-    ...    
+class Comments(db.Model):
+    __tablename__='comments'
+    
+    id = db.Column(db.Integer, primary_key= True)
+    comment = db.Column(db.Text(),nullable = False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return f'Comments {self.comment}'
+    
