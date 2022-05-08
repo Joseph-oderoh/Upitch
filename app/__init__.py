942bcdfeb8c201c3d2915fa36da6c8bb60e668bf
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-08 14:59:47 +0300

    ft/style:add auth filed and updated  migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,39 @@
 from flask import Flask
+from app import auth
+from flask_login import LoginManager
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 db = SQLAlchemy()
 
 def create_app(config_name):
 
-    app = Flask(__name__)
+  app = Flask(__name__)
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+  app.config.from_object(config_options[config_name])
 
   # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+   #main
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+    #auth 
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+  
+  
   # initializing flask apps 
-    db.init_app(app)
+  db.init_app(app)
+  login_manager.init_app(app)
+  
+  
+  
+  
     # Will add the views and forms
-    return app
+  return app
--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
-class User(db.Model):
+class User(UserMixin ,db.Model):
     __tablename__='users'
     id= db.Column(db.Integer, primary_key=True) 
     username = db.Column(db.String , unique=True, nullable=False)
-    
-    
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255))
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
     
     def __repr__(self):
         return f'User {self.username}'
@@ -19,10 +32,12 @@ class Pitch(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     category = db.Column(db.String(255), index = True,nullable = False) 
     
+    
+
     def __repr__(self):
         return f'Pitch {self.post}'
     
-class Votes(db.Model):
+class Vote(db.Model):
     __tablename__='votes'
     id = db.Column(db.Integer, primary_key= True) 
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
@@ -31,7 +46,7 @@ class Votes(db.Model):
     def __repr__(self):
         return f'{self.user_id}:{self.pitch_id}'
            
-class Comments(db.Model):
+class Comment(db.Model):
     __tablename__='comments'
     
     id = db.Column(db.Integer, primary_key= True)
