4aadcbc64b288f09fbf9972a770b14a2052e6253
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-10 14:44:11 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:oderoh@localhost/upitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -12,11 +12,30 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SUBJECT_PREFIX = 'upitch'
+    @staticmethod
+    def init_app(app):
+        pass
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI =os.environ.get ('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    pass
     
-   
+    
+    
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX = 'upitch'
+    DEBUG = True
 
+class TestConfig(Config):  
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:oderoh@localhost/upitch'
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:oderoh@localhost/upitch'
     DEBUG = True
